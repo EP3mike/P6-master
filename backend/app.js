088874de8e3125c2c03e20948d99aec7c6cf35c3
EP3mike <michaelamezcua99@gmail.com>
2022-09-29 20:12:19 -0700

    added express & mongoDB to app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // express json parser allows access to request body
 app.use(express.json());
 
+// connects to our mongoDB database
+mongoose.connect('mongodb+srv://p6-user:project6user@cluster0.kek3m1f.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
 
 // middleware to prevent a CORS ERROR(adds the right headers to allow requests from different source)
 app.use((req, res, next) => {
