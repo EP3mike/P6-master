787981938529b98ada28a0568fdfa01e7c04d407
EP3mike <michaelamezcua99@gmail.com>
2022-10-09 19:52:44 -0700

    fixed logic for set like route

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -135,8 +135,65 @@ exports.deleteSauce = (req, res, next) => {
   });
 };
 
-
 //endpoint to modify the like status for a sauce post
 exports.setLike = (req, res, next) => {
-    
+   Sauce.findOne({ _id: req.params.id }).lean().then(sauce => {
+        //case if like req is a 1(user likes sauce)
+     if (req.body.like === 1) {
+       const copyOfUserLikedArray = [...sauce.usersLiked];
+       copyOfUserLikedArray.push(req.body.userId);
+       sauce = {
+         _id: req.params.id, 
+         userId: req.body.userId, 
+         likes: sauce.likes + 1, 
+         usersLiked:copyOfUserLikedArray                           
+        };
+        //case if like req is a -1(user dislikes sauce)
+     } else if (req.body.like === -1) {
+       const copyOfUserDislikedArray = [...sauce.usersDisliked];
+       copyOfUserDislikedArray.push(req.body.userId);
+       sauce = {
+         _id: req.params.id, 
+         userId: req.body.userId, 
+         dislikes: sauce.dislikes + 1, 
+         usersDisliked: copyOfUserDislikedArray 
+        };
+        //case if user undoes their like (goes back to neutral)
+     } else {
+        //if neutral req comes from a like checks array of liked users to see if userID is included
+       if (sauce.usersLiked.includes(req.body.userId)) {
+         const indexOfUserLiked = sauce.usersLiked.indexOf(`${req.body.userId}`);
+         const copyOfUserLikedArray = [...sauce.usersLiked];
+         copyOfUserLikedArray.splice(indexOfUserLiked, 1);
+         sauce = {
+             _id: req.params.id, 
+             userId: req.body.userId, 
+             likes: sauce.likes - 1, 
+             usersLiked: copyOfUserLikedArray 
+            };
+       }
+        else if (sauce.usersDisliked.includes(req.body.userId)) {
+         const indexOfUserDisliked = sauce.usersDisliked.indexOf(req.body.userId);
+         const copyOfUserDislikedArray = [...sauce.usersDisliked];
+         copyOfUserDislikedArray.splice(indexOfUserDisliked, 1);
+         sauce = {
+             _id: req.params.id, 
+             userId: req.body.userId, 
+             dislikes: sauce.dislikes - 1, 
+             usersDisliked: copyOfUserDislikedArray 
+            };
+       }
+     }
+     Sauce.updateOne({ _id: req.params.id }, sauce)
+       .then(() => {
+         res
+           .status(201)
+           .json({
+             message: "Sauce like status submitted successfully!"
+           });
+       })
+       .catch(error => {
+         res.status(400).json({ error: error });
+       });
+   });
 };
